837040b12a6cb1491c1bbf23024b50125c724fe7
W83220719 <1483795887@qq.com>
2020-03-27 15:04:30 +0800

    不用store保存选项

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import account from './modules/account'
-import layout from './modules/layout'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    layout,
     account
   }
 })
